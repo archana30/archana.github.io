fc74bf34e3a426fe969a13c85c7cf55d63ed46de
archana30 <33727318+archana30@users.noreply.github.com>
2022-03-24 12:31:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>taxc | The Tax Consultants</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  
+  <link href="css/custom.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: Reveal
+    Theme URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body id="body">
+
+   <?php include("nav.php");?>
+  
+  
+  <!--==========================
+    Intro Section
+  ============================-->
+  
+  <section id="intro">
+	<div class="container">
+    	<div class="row">
+        <div class="col-md-8"></div>
+        	<div class="col-md-4">
+            	<h4 class="banner-title">One stop solution for all your financial needs</h4>
+                <ul class="bullet-clr">
+                	<li>Accounting</li>
+                    <li>Payroll</li>
+                    <li>HR Services</li>
+                    <li>Statutory Compliance Services</li>
+                </ul>
+                <button class="btn btn-standard"><a href="services.html">Know More</a></button>
+            </div>
+        </div>
+    </div>
+    
+
+  </section><!-- #intro -->
+
+  <main id="main">
+
+    <!--==========================
+      About Section
+    ============================-->
+    <section id="about" class="wow fadeInUp">
+      <div class="container">
+        
+        <div class="section-header">
+        <h2 class="text-center">About</h2>
+        <hr class="separator">
+          <div class="col-md-12 col-sm-12 col-lg-12">
+          	<p>taxc means The Tax Consultant. However, we didn't want to limit it to just a Traditional Tax Consultant Practise and hence several types of services have been added. taxc is specially designed to complete and deliver the results with 100% Accuracy. Utilizing our experience of more than a decade and with a team of dedicated Professionals, Prasang Management Solutions can achieve best results, which helps our clients to get out of monotonous day to day activities. <br> <a href="about.html">Read More</a></p>
+          
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #about -->
+
+    <!--==========================
+      Services Section
+    ============================-->
+    <section id="services">
+      <div class="container">
+        <div class="section-header">
+          <h2 class="text-center">Services</h2>
+          <hr class="separator">
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 service-box">
+            <div class="box wow fadeInLeft">
+              <div class="icon"><img src="img/coin-money-6.png" alt="Accounting services" width="80" height="80"></div>
+              <h4 class="text-center"><a href="">Accounting Services</a></h4>
+              <div class="overlay">
+    <div class="hover-text">At taxc we are involved in Designing Accounting Policy, Accounts Mapping, Accounts Writing & Finalization, Bank Reconciliations, Vendor Reconciliations, Billing & Invoicing etc.</div>
+  </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 service-box">
+            <div class="box wow fadeInRight">
+              <div class="icon"><img src="img/app-integration-icon.png" alt="HR services" width="80" height="80"></div>
+              <h4 class="title"><a href="">HR Services</a></h4>
+              <div class="overlay">
+    <div class="hover-text">At taxc we are involved in Designing Accounting Policy, Accounts Mapping, Accounts Writing & Finalization, Bank Reconciliations, Vendor Reconciliations, Billing & Invoicing etc.</div>
+  </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 service-box">
+            <div class="box wow fadeInLeft" data-wow-delay="0.2s">
+              <div class="icon"><img src="img/coin-money-6.png" alt="Payroll services" width="80" height="80"></div>
+              <h4 class="title"><a href="">Payroll Services</a></h4>
+              <div class="overlay">
+    <div class="hover-text">At taxc we are involved in Designing Accounting Policy, Accounts Mapping, Accounts Writing & Finalization, Bank Reconciliations, Vendor Reconciliations, Billing & Invoicing etc.</div>
+  </div>
+            </div>
+          </div>
+
+         
+
+      </div>
+   
+      <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4"><a href="services.html"><button class="btn btn-primary"><p>View all services</p></button></a></div>
+      <div class="col-md-4"></div>
+      
+   	</div>
+    </section><!-- #services -->
+
+    <!--==========================
+      Call back section
+    ============================-->
+    
+    <?php include("enquiry-form.php");?>
+    
+    <!--<div class="container-fluid back">
+    <h2 class="clbk-title">Request a call back</h2>
+    <hr class="separator">
+    
+    
+    <section id="contact" class="wow fadeInUp">
+
+      <div class="container mar-t-10">
+        <div class="form">
+          <div id="sendmessage">Your message has been sent. Thank you!</div>
+          <div id="errormessage"></div>
+          <form action="" method="post" role="form" class="contactForm">
+            <div class="form-row">
+            
+              <div class="form-group col-md-6">
+                <input type="text" name="name" class="form-control form-style" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div class="validation"></div>
+              </div>
+              
+              <div class="form-group col-md-6">
+                <input type="email" class="form-control form-style" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                
+                <div class="validation"></div>
+              </div>
+            </div>
+            
+            
+            <div class="form-group mar-b-20">
+              <textarea class="form-control form-style" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+              
+              <div class="validation"></div>
+            </div>
+            <div class="text-center"><input type="submit" name="Send Message" value="Send Message" class="btn btn-primary"></div>
+          </form>
+        </div>
+
+      </div>
+    </section><!-- #contact -->
+   <!-- <div class="container">
+    <form class="form-inline" action="/action_page.php">
+    <div class="form-group">
+      
+      <input type="name" class="form-control" id="name" placeholder="Name" name="name">
+    </div>
+    <div class="form-group">
+      
+      <input type="email" class="form-control" id="email" placeholder="Email ID" name="email">
+    </div>
+    
+    <div class="form-group">
+      
+      <input type="message" class="form-control" id="message" placeholder="Message" name="message">
+    </div>
+    
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+  </div>-->
+  
+    </div>
+
+
+ <!--==========================
+    Footer
+  ============================-->
+ <?php include("footer.php"); ?>
+
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="lib/sticky/sticky.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
